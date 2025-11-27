0de0b7750d82a9974cc7d72114ceb73a686dc3ae
ChristinaNovey1 <christinanovey@gmail.com>
2025-11-26 21:59:10 -0800

    Created main.py
    main.py has the main function and will import from all the other files as needed. Used to bring everything together

---
+++ main.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+from consts import thresholds
+from standardizer import Standardizer
+from labeler import Labeler
+
+def main():
+    # Read every food nutrition csv and put into a dataframe
+    df1 = pd.read_csv('Files/FOOD-DATA-GROUP1.csv')
+    df2 = pd.read_csv('Files/FOOD-DATA-GROUP2.csv')
+    df3 = pd.read_csv('Files/FOOD-DATA-GROUP3.csv')
+    df4 = pd.read_csv('Files/FOOD-DATA-GROUP4.csv')
+    df5 = pd.read_csv('Files/FOOD-DATA-GROUP5.csv')
+
+    # Standardize every data set by using densities
+    std = Standardizer()
+    df1 = std.standardize(df1, 'standardized_data1.csv')
+    df2 = std.standardize(df2, 'standardized_data2.csv')
+    df3 = std.standardize(df3, 'standardized_data3.csv')
+    df4 = std.standardize(df4, 'standardized_data4.csv')
+    df5 = std.standardize(df5, 'standardized_data5.csv')
+
+    # Add two new labels to every observation, health label (healthy/not healthy) and health score
+    l = Labeler(thresholds)
+    df1 = l.label(df1)
+    df2 = l.label(df2)
+    df3 = l.label(df3)
+    df4 = l.label(df4)
+    df5 = l.label(df5)
+
+    print("Labeler has logged status in logger.txt.")
+
+    # Create csv files of the DFs with labels, and save to its own folder
+    os.makedirs("Labeled_Files", exist_ok=True)
+
+    df1.to_csv("Labeled_Files/df1.csv", index=False)
+    df2.to_csv("Labeled_Files/df2.csv", index=False)
+    df3.to_csv("Labeled_Files/df3.csv", index=False)
+    df4.to_csv("Labeled_Files/df4.csv", index=False)
+    df5.to_csv("Labeled_Files/df5.csv", index=False)
+
+main()
--- preprocessor.py
+++ preprocessor.py
@@ -1,2 +1,10 @@
+import pandas as pd
+
+# Preprocessor class, will be used to preprocess the data for models
 class Preprocessor:
-    pass
+    # Constructor, initializes the data attribute with the dataset that we're preprocessing
+    def __init__(self, data : pd.DataFrame):
+        self.data = data
+
+    def preprocess(self):
+        pass
