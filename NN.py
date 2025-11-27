365569d97f96d8acbbf32ecb7d1e60e5bace7dd6
nnechi <nchio458@gmail.com>
2025-11-27 11:21:09 -0800

    added NN.py base class

---
+++ NN.py
@@ -0,0 +1,36 @@
+import sys 
+import os 
+import numpy as np 
+import pandas as pd 
+import tensorflow as tf 
+from tensorflow.keras import layers, models, optimizers
+
+class NN: 
+    def __init__(self, features, target): 
+        self.features = features; 
+        self.target = target
+
+    def train(self): 
+        
+        model = keras.Sequential(
+            [ 
+                layers.Dense(5, activation = 'relu', name = 'layer1'),
+                layers.Dense(6, activation = 'relu', name = 'layer2'),
+                layers.Dense(3, activation = 'relu', name = 'layer3'),
+                layers.Dense(1, activation='sigmoid', name = 'output')
+            ]
+        )
+        
+
+        model.compile(
+            optimizer = 'sgd', 
+            loss =- 'binarycrossentropy',
+            metrics = ['accuracy']
+        )
+
+        model.fit(self.features, self.target, epochs = 50)
+
+
+        return model
+
+
