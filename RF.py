bc5d6e9711cad95ea2879dd6371e9ea5e4f2221c
nnechi <nchio458@gmail.com>
2025-11-27 16:48:59 -0800

    Random forest default commit

--- RF.py
+++ RF.py
@@ -3,10 +3,10 @@ import os
 import numpy as np 
 import pandas as pd 
 from sklearn.ensemble import RandomForestClassifier
-#did an oopsie and committed to wrong branch. 
+
 class RandomForest: 
     def __init__(self): 
-        self.model = RandomForestClassifier(n_estimators=60, max_depth=None, random_state=42)
+        self.model = RandomForestClassifier(n_estimators=60, max_depth=None, random_state=42) #Maybe we need tro switch the n estimators here. 
 
     def train(self, xtrain, ytrain): 
         self.model.fit(xtrain, ytrain)
